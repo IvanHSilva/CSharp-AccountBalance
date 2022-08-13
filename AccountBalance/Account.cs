c8c874ad9f27bce274096ea13e0ec7428b666010
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-08-13 14:03:03 -0300

    Add project files.

---
+++ AccountBalance/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountBalance {
+    public class Account {
+
+        // attributes
+        public int Number { get; private set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+
+        // constructors
+        public Account(int number, string holder) {
+            Number = number;
+            Holder = holder;
+        }
+
+        public Account(int number, string holder, double deposit) : this(number, holder) {
+            Deposit(deposit);
+        }
+
+        // methods
+        private double tax = 5.0;
+
+        public void Deposit(double amount) {
+            Balance += amount;
+        }
+        
+        public void Withdraw(double amount) {
+            Balance -= amount + tax;
+        }
+
+        // tostring
+        public override string ToString() {
+            return $"Conta: {Number}, Titular: {Holder}, Saldo: $ {Balance.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ AccountBalance/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountBalance {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // variables declaration
+            Account acc;
+
+            // data solicitation
+            Console.Write("Digite o número da conta: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Digite o titular da conta: ");
+            string holder= Console.ReadLine();
+            Console.Write("Tem depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+
+            // Check initial deposit
+            if (resp == 's' || resp == 'S') {
+                Console.Write("Valor do depósito inicial: ");
+                double deposit = double.Parse(Console.ReadLine());
+                acc = new Account(number, holder, deposit);
+            }
+            else {
+                acc = new Account(number, holder);
+            }
+
+
+            // Show account data
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(acc);
+            Console.WriteLine();
+
+            // Operations
+            Console.Write("Valor do Depósito: ");
+            double amount = double.Parse(Console.ReadLine());
+            acc.Deposit(amount);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(acc);
+            Console.WriteLine();
+
+            Console.Write("Valor do Saque: ");
+            amount = double.Parse(Console.ReadLine());
+            acc.Withdraw(amount);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(acc);
+        }
+    }
+}
